93c822d6e45755af13bc011de805eaa9151ea513
vanesvorld <white@electric.mygbiz.com>
2019-02-05 18:37:55 +0000

    Add MIPS32 toolchain

---
+++ toolchains/mips32/lib/gcc/mips-buildroot-linux-uclibc/7.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/home/sysop/devel/buildroot/output/host --sysconfdir=/home/sysop/devel/buildroot/output/host/etc --enable-static --target=mips-buildroot-linux-uclibc --with-sysroot=/home/sysop/devel/buildroot/output/host/mips-buildroot-linux-uclibc/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/home/sysop/devel/buildroot/output/host --with-mpc=/home/sysop/devel/buildroot/output/host --with-mpfr=/home/sysop/devel/buildroot/output/host --with-pkgversion='Buildroot 2019.02-git-00975-g9dcb5c581b' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --disable-libsanitizer --enable-tls --disable-libmudflap --enable-threads --without-isl --without-cloog --with-float=soft --with-arch=mips32r2 --with-abi=32 --with-nan=legacy --enable-languages=c --with-build-time-tools=/home/sysop/devel/buildroot/output/host/mips-buildroot-linux-uclibc/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "32" }, { "arch", "mips32r2" }, { "float", "soft" }, { "nan", "legacy" }, { "llsc", "llsc" } };
